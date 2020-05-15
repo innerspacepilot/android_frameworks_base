84f1468698228f63f4baeebfcc098a765cae4beb
innerspacepilot <innerspacepilot@gmx.com>
2020-05-15 17:08:16 +0300

    om shanti

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -165,15 +165,8 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
-            v.setVisibility(View.VISIBLE);
-        } else {
-            v.setVisibility(View.GONE);
-        }
+    		v.setText("ॐ शान्तिः");
+        v.setVisibility(View.VISIBLE);
     }
 
     private void updateAnimator(int width) {
